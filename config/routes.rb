a9e2b0a6a6edfb9ee93ab502a7a7aabcca7c9dcf
Sharon Connell <sharon.connell@careerbuilder.com>
2015-03-06 16:48:37 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'sections' => 'welcome#sections'
   get 'works' => 'works#index'
   
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
